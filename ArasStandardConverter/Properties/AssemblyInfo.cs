7abe017f86c7ea58c4da0de0e38cc2c9a2223215
Adam Rhoda <adrho@microsoft.com>
2018-08-15 08:43:37 -0700

    Updated AssemblyInfo

--- ArasStandardConverter/Properties/AssemblyInfo.cs
+++ ArasStandardConverter/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OnePDMStandardConverter")]
+[assembly: AssemblyTitle("ArasStandardConverter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OnePDMStandardConverter")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyProduct("ArasStandardConverter")]
+[assembly: AssemblyCopyright("Microsoft Corporation © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
